2e09fe852be3e24facaa0e238605895490c2d644
Joshua Geissler <jgeissler14@gmail.com>
2024-10-19 12:05:39 -0700

    zip

--- cmd/backup.go
+++ cmd/backup.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +13,7 @@ import (
 var (
 	source string
 	target string
-	zip    bool
+	tozip  string
 )
 
 var backupCmd = &cobra.Command{
@@ -21,10 +24,17 @@ var backupCmd = &cobra.Command{
 			fmt.Println("Error: --source and --target are required.")
 			os.Exit(1)
 		}
-		fmt.Printf("Starting backup...\nSource: %s\nTarget: %s\n", source, target)
-		if zip {
-			fmt.Printf("Zipping Files...\n")
+
+		// Check if --tozip is provided and if it ends with ".zip"
+		if tozip != "" {
+			if !strings.HasSuffix(tozip, ".zip") {
+				fmt.Println("Error: --tozip must specify a file with a .zip extension.")
+				os.Exit(1)
+			}
+			fmt.Printf("Zipping files to %s...\n", tozip)
 		}
+
+		fmt.Printf("Starting backup...\nSource: %s\nTarget: %s\n", source, target)
 
 		files, err := os.Open(source)
 		if err != nil {
@@ -39,8 +49,40 @@ var backupCmd = &cobra.Command{
 			return
 		}
 
-		for _, file := range fileInfo {
-			fmt.Println(file.Name())
+		if len(tozip) > 0 {
+			fmt.Printf("Zipping Files...\n")
+
+			archive, err := os.Create(tozip)
+			defer archive.Close()
+			zipWriter := zip.NewWriter(archive)
+			if err != nil {
+				panic(err)
+			}
+
+			for _, file := range fileInfo {
+				if file.IsDir() {
+					return
+				}
+				fmt.Printf("writing %s to archive...\n", file.Name())
+
+				f, err := os.Open(file.Name())
+				if err != nil {
+					panic(err)
+				}
+				defer f.Close()
+
+				w, err := zipWriter.Create(file.Name() + ".zip")
+				if err != nil {
+					panic(err)
+				}
+
+				if _, err := io.Copy(w, f); err != nil {
+					panic(err)
+				}
+
+				fmt.Println("closing zip archive...")
+				zipWriter.Close()
+			}
 		}
 	},
 }
@@ -49,7 +91,7 @@ func init() {
 	// Add flags for the backup command
 	backupCmd.Flags().StringVarP(&source, "source", "s", "", "Source directory to backup")
 	backupCmd.Flags().StringVarP(&target, "target", "t", "", "Target location for the backup")
-	backupCmd.Flags().BoolVarP(&zip, "zip", "z", false, "Zip the source before backup")
+	backupCmd.Flags().StringVarP(&tozip, "tozip", "z", "", "Zip the source before backup")
 
 	// Mark --source and --target as required
 	backupCmd.MarkFlagRequired("source")
